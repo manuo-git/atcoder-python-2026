ed1f2bedb7a66373559f6b6aef785d3e43273069
manuo-git <saintmanuo@gmail.com>
2026-01-10 14:54:47 +0900

    snipetを更新

--- src/python/lib/updatesnipet.py
+++ src/python/lib/updatesnipet.py
@@ -3,5 +3,5 @@ lines = []
 with open("C:\\Users\\saintmanuo\\AppData\\Roaming\\Code\\User\\snippets\\python.json", encoding="utf-8") as f:
     lines = f.readlines()
 
-with open("D:\\PythonScripts\\atcoder\\python\\snippets.json", "w", encoding="utf-8") as f:
+with open("D:\\PythonScripts\\atcoder\\python\\src\\python\\snippets.json", "w", encoding="utf-8") as f:
     f.writelines(lines)
