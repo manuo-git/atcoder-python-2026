947c2ea00e5822d733e73f32818ee96befe19ed0
manuo-git <saintmanuo@gmail.com>
2026-03-18 14:30:15 +0900

    codonでdfsが含まれていても警告を出さないように

--- src/python/lib/paste2clip.py
+++ src/python/lib/paste2clip.py
@@ -20,12 +20,14 @@ from typing import List
 with open("answer.py", "r", encoding="utf-8") as f:
     lines: List[str] = f.readlines()
     dfsflag = False
+    codonflag = False
     pypyflag = False
     for line in lines:
         if "dfs" in line: dfsflag = True
+        if "@extend" in line: codonflag = True
         if "pypy" in line: pypyflag = True
 
-    if dfsflag and not pypyflag:
+    if dfsflag and not codonflag and not pypyflag:
         for s in saikikeisatu:
             print(s)
         print("Detected \"dfs\". Might need a spell. Copied.")
